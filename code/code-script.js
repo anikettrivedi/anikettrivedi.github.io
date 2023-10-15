21d31ffd0277001d27afdb067f79c100a47d182f
Aniket Trivedi <aniket@Anikets-MacBook-Pro.local>
2023-10-15 17:45:43 +0530

    code

--- code/code-script.js
+++ code/code-script.js
@@ -1,4 +1,27 @@
-console.log("hello")
+var commandsJson;
+
 fetch('https://anikettrivedi.github.io/code/commands.json')
     .then((response) => response.json())
-    .then((json) => console.log(json));
+    .then((json) => {
+        commandsJson = json;
+    })
+    .then(()=> {
+        insertCommands()
+    });
+
+function insertCommands(){
+    commandsJson.forEach(element => {
+        insertCommandSection(element)
+    });
+}
+
+function insertCommandSection(element){
+    var commandContainer = document.getElementById("command-container")
+    var div = document.createElement("div")
+    var h3 = document.createElement("h3")
+    var text = document.createTextNode(element.title)
+
+    h3.appendChild(text)
+    div.appendChild(h3)
+    commandContainer.appendChild(div)
+}

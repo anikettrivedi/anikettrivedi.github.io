1539d0f7f42bf34e5b4a4277847e38eaf48e1322
Aniket Trivedi <aniket@Anikets-MacBook-Pro.local>
2023-10-24 13:06:50 +0530

    asdf

--- code/notes/code-script.js
+++ code/notes/code-script.js
@@ -1,6 +1,6 @@
 var commandsJson;
 
-fetch('https://anikettrivedi.github.io/code/commands.json')
+fetch('https://anikettrivedi.github.io/code/assets/commands.json')
     .then((response) => response.json())
     .then((json) => {
         commandsJson = json;
@@ -19,9 +19,9 @@ function insertCommandSection(element){
     var commandContainer = document.getElementById("command-container")
     var div = document.createElement("div")
     var h3 = document.createElement("h3")
-    var text = document.createTextNode(element.title)
-
-    h3.appendChild(text)
+    var title = document.createTextNode(element.title)
+    var link = element.link
+    h3.appendChild(title)
     div.appendChild(h3)
     commandContainer.appendChild(div)
 }

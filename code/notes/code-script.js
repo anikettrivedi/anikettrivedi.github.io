816c65257996cb1eb98c4d7c7672dd9e537675ea
Aniket Trivedi <aniket@Anikets-MacBook-Pro.local>
2023-10-24 14:03:35 +0530

    committed on Tue Oct 24 14:03:35 IST 2023

--- code/notes/code-script.js
+++ code/notes/code-script.js
@@ -1,4 +1,6 @@
 var titleLinkMap;
+var selectContainer = document.getElementById("select-container")
+var commandContainer = document.getElementById("command-container")
 
 fetch('https://anikettrivedi.github.io/code/assets/commands.json')
     .then((response) => response.json())
@@ -6,25 +8,54 @@ fetch('https://anikettrivedi.github.io/code/assets/commands.json')
         titleLinkMap = json;
     })
     .then(()=> {
-        insertSections()
+        // create a drop down
+        insertSelect()
+        
+        // insert selected section only
+        insertSelectedSection()
     });
 
-function insertSections(){
+function insertSelect(){
+    let select = document.createElement("select")
+    select.setAttribute("name", "command-type")
+    select.setAttribute("id", "select-command-type")
+    select.onchange = insertSelectedSection
+    selectContainer.appendChild(select)
+
+    titleLinkMap.forEach(
+        (i) => {
+            let option = document.createElement("option")
+            option.setAttribute("value", i.title)
+            option.appendChild(document.createTextNode(i.title))
+            select.appendChild(option)
+        }
+    )
+}
+
+function insertSelectedSection(){
+    // get selected section
+    let selectedSection = document.getElementById("select-command-type").value
+    console.log(selectedSection)
     titleLinkMap.forEach(i => {
-        fetchSectionContentsAndAdd(i)
+        if (i.title === selectedSection){
+            fetchSectionContentsAndAdd(i)
+        }
     });
 }
 
-function fetchSectionContentsAndAdd(i){
-    var commandContainer = document.getElementById("command-container")
-    var h3 = document.createElement("h3")
-    h3.appendChild(document.createTextNode(i.title))
-    commandContainer.appendChild(h3)
-    
-    var link = i.link
-    fetch(link)
+function fetchSectionContentsAndAdd(i){    
+    fetch(i.link)
         .then((response) => response.text())
         .then((content) => {
+            // create container contents
+            commandContainer.innerHTML = ""
+
+            // add title
+            let h3 = document.createElement("h3")
+            h3.appendChild(document.createTextNode(i.title))
+            commandContainer.appendChild(h3)
+    
+            // add contents
             content.split("\n").forEach(
                 (line) => {
                     // add sub heading for the section
